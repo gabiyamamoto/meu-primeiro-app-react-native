65b1302fc96a5cb0d6b0f5c57fc732fb58c33e27
gabi emi <gabiemi2021@gmail.com>
2026-02-08 21:15:25 -0300

    Estiliza a tela

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Image, Button, TextInput, Pressable } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.body}>
       <View style={styles.header}>
-        <Image
-          source={require("./assets/chapeu.png")}
-          style={styles.chapeu}
-        />
+        <View style={styles.sombraChapeu}>
+          <Image
+            source={require("./assets/chapeu.png")}
+            style={styles.chapeu}
+          />
+        </View>
       </View>
 
       <View style={styles.main}>
@@ -29,15 +31,17 @@ export default function App() {
         </View>
 
         <View style={styles.secaoEsqueci}>
-          <Text style={styles.esqueciSenha}>Esqueci minha senha</Text>
+          <Text style={styles.textoRoxo}>Esqueci minha senha</Text>
         </View>
 
         <View style={styles.secaoBotao}>
-          <Button title="Entrar"></Button>
+            <Pressable style={styles.botao}>
+              <Text style={styles.textoBotao}>Entrar</Text>
+            </Pressable>
         </View>
 
         <View style={styles.secaoEntreCom}>
-          <Text style={styles.entreCom}>Ou entre com</Text>
+          <Text style={styles.textoCinza}>Ou entre com</Text>
         </View>
 
         <View style={styles.secaoPlataformas}>
@@ -56,20 +60,160 @@ export default function App() {
         </View>
 
         <View style={styles.secaoCadastrar}>
-          <Text style={styles.entreCom}>Não tem uma conta?</Text>
-          <Text style={styles.Cadastrar}>Cadastre-se</Text>
+          <Text style={styles.textoCinza}>Não tem uma conta?</Text>
+          <Text style={styles.textoRoxo}>Cadastre-se</Text>
         </View>
-        
+
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fffcfc',
   },
+
+  header: {
+    width: '100%',
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    marginTop: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  chapeu: {
+    width: 80,
+    height: 80,
+    resizeMode: 'contain',
+  },
+
+  sombraChapeu: {
+    width: 96,
+    height: 96,
+    borderRadius: 20,
+    backgroundColor: "#fffcfc",
+    justifyContent: "center",
+    alignItems: "center",
+
+    shadowColor: "#7C3AED",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 12,
+
+    elevation: 15,
+  },
+
+  main: {
+    padding: 25,
+    flex: 1,
+  },
+
+  bemVindo: {
+    fontSize: 25,
+    fontWeight: '600',
+  },
+
+  subtitulo: {
+    marginTop: 8,
+    marginBottom: 15,
+    fontSize: 15,
+    color: '#808080',
+    fontWeight: '600',
+  },
+
+  secaoLogin: {
+    marginTop: 7,
+    marginBottom: 5,
+  },
+
+  label: {
+    fontWeight: '600',
+  },
+
+  input: {
+    width: '97%',
+    height: 45,
+    marginVertical: 10,
+    marginHorizontal: 5,
+    borderWidth: 2,
+    borderColor: '#f3efef',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+  },
+
+  secaoEsqueci: {
+    alignItems: 'flex-end',
+    marginHorizontal: 5,
+    marginBottom: 7,
+  },
+
+  textoRoxo: {
+    color: '#7529b8',
+    fontWeight: '600',
+  },
+
+  secaoBotao: {
+    marginVertical: 10,
+  },
+
+  botao: {
+    backgroundColor: '#6721A5',
+    height: 48,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  textoBotao: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+
+  secaoEntreCom: {
+    marginVertical: 15,
+    alignItems: 'center',
+  },
+
+  textoCinza: {
+    color: '#808080',
+    fontWeight: '600',
+  },
+
+  secaoPlataformas: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 10,
+    gap: 40,
+  },
+
+  github: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+  },
+
+  google: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+  },
+
+  insta: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+  },
+
+  secaoCadastrar: {
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 5,
+  },
+
 });
