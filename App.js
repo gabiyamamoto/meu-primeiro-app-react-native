adbb448d19f37436a18016a3c8bd980dd2d6a364
gabi emi <gabiemi2021@gmail.com>
2026-02-13 09:50:50 -0300

    style: adiciona icones, linhas e sombra
    - Adiciona icons nos inputs
    - Adiciona linhas na secao entreCom
    - Adiciona sombra no botão de entrar

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, Button, TextInput, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -21,13 +29,35 @@ export default function App() {
         </View>
         <View style={styles.secaoLogin}>
           <Text style={styles.label}>E-mail</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="seu@email.com"
-          ></TextInput>
-
+          <View style={styles.inputContainer}>
+            <FontAwesome
+              name="envelope"
+              size={15}
+              color="#a3a3a3"
+              style={{ marginRight: 10 }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="seu@email.com"
+              placeholderTextColor="#979797"
+            ></TextInput>
+          </View>
           <Text style={styles.label}>Senha</Text>
-          <TextInput style={styles.input} placeholder="********"></TextInput>
+          <View style={styles.inputContainer}>
+            <FontAwesome
+              name="lock"
+              size={18}
+              color="#a3a3a3"
+              style={{ marginRight: 10 }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="********"
+              placeholderTextColor="#979797"
+              secureTextEntry={true}
+            ></TextInput>
+            <FontAwesome name="eye" size={18} color="#a3a3a3" />
+          </View>
         </View>
 
         <View style={styles.secaoEsqueci}>
@@ -35,35 +65,42 @@ export default function App() {
         </View>
 
         <View style={styles.secaoBotao}>
-            <Pressable style={styles.botao}>
-              <Text style={styles.textoBotao}>Entrar</Text>
-            </Pressable>
+          <TouchableOpacity style={styles.botao} activeOpacity={0.7}>
+            <Text style={styles.textoBotao}>Entrar →</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.secaoEntreCom}>
+          <View style={styles.line} />
           <Text style={styles.textoCinza}>Ou entre com</Text>
+          <View style={styles.line} />
         </View>
 
         <View style={styles.secaoPlataformas}>
-          <Image
-            source={require("./assets/github.png")}
-            style={styles.github}
-          />
-          <Image
-            source={require("./assets/google.png")}
-            style={styles.google}
-          />
-          <Image
-            source={require("./assets/insta.png")}
-            style={styles.insta}
-          />
+          <View style={styles.iconsWrapper}>
+            <Image
+              source={require("./assets/github.png")}
+              style={styles.github}
+            />
+          </View>
+          <View style={styles.iconsWrapper}>
+            <Image
+              source={require("./assets/google.png")}
+              style={styles.google}
+            />
+          </View>
+          <View style={styles.iconsWrapper}>
+            <Image
+              source={require("./assets/insta.png")}
+              style={styles.insta}
+            />
+          </View>
         </View>
 
         <View style={styles.secaoCadastrar}>
           <Text style={styles.textoCinza}>Não tem uma conta?</Text>
           <Text style={styles.textoRoxo}>Cadastre-se</Text>
         </View>
-
       </View>
     </View>
   );
@@ -72,11 +109,11 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#fffcfc',
+    backgroundColor: "#F7F9FA",
   },
 
   header: {
-    width: '100%',
+    width: "100%",
     paddingVertical: 16,
     paddingHorizontal: 24,
     marginTop: 70,
@@ -87,23 +124,23 @@ const styles = StyleSheet.create({
   chapeu: {
     width: 80,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
 
   sombraChapeu: {
     width: 96,
     height: 96,
     borderRadius: 20,
-    backgroundColor: "#fffcfc",
+    backgroundColor: "#F7F9FA",
     justifyContent: "center",
     alignItems: "center",
 
-    shadowColor: "#7C3AED",
+    shadowColor: "#985dff",
     shadowOffset: { width: 0, height: 8 },
     shadowOpacity: 0.25,
     shadowRadius: 12,
 
-    elevation: 15,
+    elevation: 10,
   },
 
   main: {
@@ -113,15 +150,15 @@ const styles = StyleSheet.create({
 
   bemVindo: {
     fontSize: 25,
-    fontWeight: '600',
+    fontWeight: "600",
   },
 
   subtitulo: {
     marginTop: 8,
     marginBottom: 15,
     fontSize: 15,
-    color: '#808080',
-    fontWeight: '600',
+    color: "#808080",
+    fontWeight: "600",
   },
 
   secaoLogin: {
@@ -130,29 +167,36 @@ const styles = StyleSheet.create({
   },
 
   label: {
-    fontWeight: '600',
+    fontWeight: "600",
   },
 
-  input: {
-    width: '97%',
-    height: 45,
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    borderWidth: 2,
+    borderColor: "#eeeeee",
+    borderRadius: 10,
     marginVertical: 10,
     marginHorizontal: 5,
-    borderWidth: 2,
-    borderColor: '#f3efef',
-    borderRadius: 10,
     paddingHorizontal: 20,
   },
 
+  input: {
+    flex: 1,
+    height: 45,
+    fontWeight: "600",
+  },
+
   secaoEsqueci: {
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
     marginHorizontal: 5,
     marginBottom: 7,
   },
 
   textoRoxo: {
-    color: '#7529b8',
-    fontWeight: '600',
+    color: "#7529b8",
+    fontWeight: "600",
   },
 
   secaoBotao: {
@@ -160,60 +204,84 @@ const styles = StyleSheet.create({
   },
 
   botao: {
-    backgroundColor: '#6721A5',
+    backgroundColor: "#6721A5",
     height: 48,
     borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#5d00ff",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 12,
+
+    elevation: 10,
   },
 
   textoBotao: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: "600",
   },
 
   secaoEntreCom: {
+    flexDirection: "row",
     marginVertical: 15,
-    alignItems: 'center',
+    alignItems: "center",
+    gap: 13,
+  },
+
+  line: {
+    flex: 1,
+    height: 2,
+    backgroundColor: "#ebebeb",
   },
 
   textoCinza: {
-    color: '#808080',
-    fontWeight: '600',
+    color: "#808080",
+    fontWeight: "600",
   },
 
   secaoPlataformas: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginVertical: 10,
-    gap: 40,
+    gap: 30,
+  },
+
+  iconsWrapper: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: "#eeeeee",
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   github: {
-    width: 40,
-    height: 40,
-    resizeMode: 'contain',
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
   },
 
   google: {
-    width: 40,
-    height: 40,
-    resizeMode: 'contain',
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
   },
 
   insta: {
-    width: 40,
-    height: 40,
-    resizeMode: 'contain',
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
   },
 
   secaoCadastrar: {
     marginTop: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
     gap: 5,
   },
-
 });
